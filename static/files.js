c88b6e2f34c7578b1d546646cadb665c29a59003
CTRLDON <a.m.elatawy@gmail.com>
2024-11-16 00:08:47 +0200

    adding upload button and styling it

---
+++ static/files.js
@@ -0,0 +1,8 @@
+// Function to toggle the dropdown menu
+
+
+// // Logout functionality
+// document.getElementById("logout-btn").onclick = function() {
+//     alert("Logging out...");
+//     // Add your logout logic here
+// };
